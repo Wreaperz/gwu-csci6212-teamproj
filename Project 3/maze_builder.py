18bd6be6f4a0d723e7339b4fde4c58cf5f788f24
NathanOrmsby <nathan.ormsby.19@cnu.edu>
2023-10-31 09:45:34 -0700

    Working on algorithm to add branches

--- Project 3/maze_builder.py
+++ Project 3/maze_builder.py
@@ -308,6 +308,224 @@ def singularize_maze(maze):
                 maze[i][j] = '#'
     return maze
 
+def add_branches_to_maze(maze, num_branches):
+    # Store all paths into a set
+    path_locations = []  # Stores coordinates of locations in the maze where there is a path
+    for i in range(len(maze)):
+        for j in range(len(maze[0])):
+            if maze[i][j] == '.':
+                path_locations.append([i, j])
+    path_locations = set(path_locations)
+    
+    max_branch_size = len(maze)
+    min_branch_size = len(maze / 4)
+
+    directions = {"left": (-1, 0), "right": (1, 0), "down": (0, -1), "up": (0, 1)}
+    opposite_directions = {"left": "right", "right": "left", "up": "down", "down": "up"}
+
+    for branch in range(num_branches):
+
+        # Pick a random starting location
+        starting_y, starting_x = random.choice(path_locations)
+
+        # Remove all adjacent locations to starting position: Split into x and y to prevent confusion
+        starting_adjacent_left_x, starting_adjacent_left_y = starting_x - 1, starting_y
+        starting_adjacent_right_x, starting_adjacent_right_y = starting_x + 1, starting_y
+        starting_adjacent_above_x, starting_adjacent_above_y = starting_x, starting_y + 1
+        starting_adjacent_below_x, starting_adjacent_below_y = starting_x, starting_y - 1
+
+        starting_adjacent_left = [starting_adjacent_left_y, starting_adjacent_left_x]
+        starting_adjacent_right = [starting_adjacent_right_y, starting_adjacent_right_x]
+        starting_adjacent_above = [starting_adjacent_above_y, starting_adjacent_above_x]
+        starting_adjacent_below = [starting_adjacent_below_y, starting_adjacent_below_x]
+
+        starting_adjacents = [starting_adjacent_left, starting_adjacent_right, starting_adjacent_above, starting_adjacent_below]
+
+        ### ------------------------------------- ### Continue debugging here
+
+        for starting_adjacent in starting_adjacents:
+            if starting_adjacent in path_locations:
+                path_locations.remove(starting_adjacent)
+
+        # Pick a random branch length
+        path_length = random.randint(min_branch_size, max_branch_size)
+        current_length = 0
+
+        # Directions record
+        previous_direction = None
+        current_direction = None
+
+        # Define current location and adjacents
+        current_x = None
+        current_y = None
+        current_adjacent_left_x, current_adjacent_left_y = None, None
+        current_adjacent_right_x, current_adjacent_right_y = None, None
+        current_adjacent_above_x, current_adjacent_above_y = None, None
+        current_adjacent_below_x, current_adjacent_below_y = None, None
+        
+        # Travel through the entire path
+        stop = False
+        while current_length < path_length or not stop:
+            # List of direction possibilities
+            direction_possibilities = ["left", "right", "up", "down"]
+
+            # If just starting
+            if current_direction is None:
+                # Pick a direction based on starting_adjacents to current location
+                # If adjacents are on exterior of maze
+                # Adjacent left
+                if starting_adjacent_left[0] == 0 or starting_adjacent_left[1] == 0 or starting_adjacent_left[1] == len(maze) - 1:
+                    direction_possibilities.remove("left")
+                # Adjacent right
+                if starting_adjacent_right[0] == len(maze) - 1 or starting_adjacent_right[1] == 0 or starting_adjacent_right[1] == len(maze) - 1:
+                    direction_possibilities.remove("right")
+                # Adjacent above
+                if starting_adjacent_above[0] == 0 or starting_adjacent_above[0] == len(maze) - 1 or starting_adjacent_above[1] == 0:
+                    direction_possibilities.remove("up")
+                # Adjacent below
+                if starting_adjacent_below[0] == 0 or starting_adjacent_below[0] == len(maze) - 1 or starting_adjacent_below[1] == len(maze) - 1:
+                    direction_possibilities.remove("down")
+                
+                # Dont travel into previous path
+                # Left
+                if maze[starting_adjacent_left[0]][starting_adjacent_left[1]] == '.':
+                    if "left" in direction_possibilities:
+                        direction_possibilities.remove("left")
+                # Right
+                if maze[starting_adjacent_right[0]][starting_adjacent_right[1]] == '.':
+                    if "right" in direction_possibilities:
+                        direction_possibilities.remove("right")
+                # Above
+                if maze[starting_adjacent_above[0]][starting_adjacent_above[1]] == '.' or maze[starting_adjacent_above[0]][starting_adjacent_above[1]] == 'S':
+                    if "up" in direction_possibilities:
+                        direction_possibilities.remove("up")
+                # Below
+                if maze[starting_adjacent_below[0]][starting_adjacent_below[1]] == '.' or maze[starting_adjacent_below[0]][starting_adjacent_below[1]] == 'E':
+                    if "down" in direction_possibilities:
+                        direction_possibilities.remove("down")
+                
+                # Check to make sure there are still possible directions
+                if len(direction_possibilities) == 0:
+                    print("NO POSSIBLE DIRECTIONS TO TRAVEL: NEED TO INVESTIGATE AND HANDLE THIS CASE")
+                    print(f"Location is: ({starting_y}, {starting_x})")
+                    exit(1)
+                
+                # Randomly pick a remaining direction to travel
+                current_direction = random.choice(direction_possibilities)
+                dx, dy = directions[current_direction]
+
+                # Travel to new location
+                current_x = starting_x + dx
+                current_y = starting_y + dy
+
+                # Define new adjacents
+                current_adjacent_left = [current_y, current_x - 1]
+                current_adjacent_right = [current_y, current_x + 1]
+                current_adjacent_above = [current_y + 1, current_x]
+                current_adjacent_below = [current_y - 1, current_x]
+
+                # Increment path length
+                current_length += 1
+                
+            # If continuing along path
+            else:
+                # Consider previous direction if applicable
+                if previous_direction is not None:
+                    if previous_direction != current_direction:
+                        direction_possibilities.remove(previous_direction)
+                
+                # Dont travel opposite to current direction
+                direction_possibilities.remove(opposite_directions[current_direction])
+
+                # Pick a direction based on starting_adjacents to current location
+                # If adjacents are on exterior of maze
+                # Adjacent left
+                if current_adjacent_left[0] == 0 or current_adjacent_left[1] == 0 or current_adjacent_left[1] == len(maze) - 1:
+                    if "left" in direction_possibilities:
+                        direction_possibilities.remove("left")
+                # Adjacent right
+                if current_adjacent_right[0] == len(maze) - 1 or current_adjacent_right[1] == 0 or current_adjacent_right[1] == len(maze) - 1:
+                    if "right" in direction_possibilities:
+                        direction_possibilities.remove("right")
+                # Adjacent above
+                if current_adjacent_above[0] == 0 or current_adjacent_above[0] == len(maze) - 1 or current_adjacent_above[1] == 0:
+                    if "up" in direction_possibilities:
+                        direction_possibilities.remove("up")
+                # Adjacent below
+                if current_adjacent_below[0] == 0 or current_adjacent_below[0] == len(maze) - 1 or current_adjacent_below[1] == len(maze) - 1:
+                    if "down" in direction_possibilities:
+                        direction_possibilities.remove("down")
+                
+                # Dont travel into previous path
+                # Left
+                if maze[current_adjacent_left[0]][current_adjacent_left[1]] == '.':
+                    if "left" in direction_possibilities:
+                        direction_possibilities.remove("left")
+                # Right
+                if maze[current_adjacent_right[0]][current_adjacent_right[1]] == '.':
+                    if "right" in direction_possibilities:
+                        direction_possibilities.remove("right")
+                # Above
+                if maze[current_adjacent_above[0]][current_adjacent_above[1]] == '.' or maze[current_adjacent_above[0]][current_adjacent_above[1]] == 'S':
+                    if "up" in direction_possibilities:
+                        direction_possibilities.remove("up")
+                # Below
+                if maze[current_adjacent_below[0]][current_adjacent_below[1]] == '.' or maze[current_adjacent_below[0]][current_adjacent_below[1]] == 'E':
+                    if "down" in direction_possibilities:
+                        direction_possibilities.remove("down")
+
+                # Check to make sure there are still possible directions if not end path short
+                if len(direction_possibilities) == 0:
+                    print("NO POSSIBLE DIRECTIONS TO TRAVEL: ENDING PATH SHORT")
+                    print(f"Location is: ({starting_y}, {starting_x})")
+                    stop = True
+
+                # Slight Bias traveling in the current direction to avoid excessive zagging
+                if current_direction in direction_possibilities:
+                    for i in range(2):
+                        direction_possibilities.append(current_direction)
+
+                # Randomly pick a remaining direction to travel
+                previous_direction = current_direction
+                current_direction = random.choice(direction_possibilities)
+                dx, dy = directions[current_direction]
+
+                # Travel to new location
+                current_x = starting_x + dx
+                current_y = starting_y + dy
+
+                # Define new adjacents
+                current_adjacent_left = [current_y, current_x - 1]
+                current_adjacent_right = [current_y, current_x + 1]
+                current_adjacent_above = [current_y + 1, current_x]
+                current_adjacent_below = [current_y - 1, current_x]
+
+                # Increment path length
+                current_length += 1
+
+
+
+
+
+
+
+
+                
+                
+
+
+            
+                        
+
+
+
+
+
+
+
+    
+
+
 
 def print_maze(maze):
     for row in maze:
